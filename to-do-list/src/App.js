28da9cdb843353628a0a2792e447a4d087b9c1fd
[HiruSam] <[hirusamarawickrama824hs@gmail.com]>
2020-05-07 21:58:04 +0530

    NavigationBar.js added

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import TodoMain from "./components/TodoMain";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
     return (
         <div className="App" >
+            {/*<NavigationBar/>*/}
             <TodoMain/>
         </div>
     );
